ec9bea2c44cdfe6926686824e4436d146b1bc011
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2013-03-20 23:58:34 +0200

    Init commit for ErrOR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErrOR::Application.config.secret_token = '72af148b410a398cc1cd16c9cdd15653b951f3bc365b076418f3f272b7ff871f04f834aec38f50129c106d1c823532c4d1c6304f43d9c4484eae94965ade93f8'
